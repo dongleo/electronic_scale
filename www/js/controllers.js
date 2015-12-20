b63b4dbba4c79fb50bd8dd53cf9fb93cbc451d35
liudong0906@gmail.com <liudong0906@gmail.com>
2015-12-20 19:17:29 +0800

    Account list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,8 @@ controllers.value('Conf', {
     'waistlineDic': [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180],
     'SERVICE_UUID': 'FFF0',
     'CHARACTERISTIC_UUID': 'FFF6',
-    'LOCKED_FLAT': -47
+    'LOCKED_FLAT': -47,
+    'SCAN_TIMEOUT': 10
 });
 
 controllers.controller('loginCtrl', function ($scope, $state, $ionicPopup, $ionicLoading, StorageHelper, AccountService) {
@@ -137,7 +138,7 @@ controllers.controller('loginCtrl', function ($scope, $state, $ionicPopup, $ioni
         };
         $scope.scan = function () {
             //$cordovaBLE.scan([], 10).then(function (device) {
-            ble.scan([], 10, function (device) {
+            ble.scan([], Conf.SCAN_TIMEOUT, function (device) {
                 $ionicPopup.alert({
                     title: 'scan',
                     template: 'scan'
@@ -185,7 +186,7 @@ controllers.controller('loginCtrl', function ($scope, $state, $ionicPopup, $ioni
         $ionicPlatform.ready($scope.init);
     })
 
-    .controller('bleListCtrl', function ($scope, $state, $ionicPlatform, $ionicLoading, $ionicPopup, $timeout, $cordovaBLE, BleManager) {
+    .controller('bleListCtrl', function ($scope, $state, $ionicPlatform, $ionicLoading, $ionicPopup, $timeout, $cordovaBLE, BleManager, Conf) {
         $scope.bleList = [];
         $scope.isBleSelected = function () {
             return $scope.selectedBle == undefined || $scope.selectedBle == null;
@@ -208,17 +209,21 @@ controllers.controller('loginCtrl', function ($scope, $state, $ionicPopup, $ioni
         $scope.scan = function () {
             //TODO $cordovaBLE收不到回调
             //$cordovaBLE.scan([], 10).then(function (device) {
-            ble.scan([], 10, function (device) {
+            ble.scan([], Conf.SCAN_TIMEOUT, function (device) {
                 $ionicLoading.hide();
                 $scope.scanCallback(device);
             }, function () {
-                $ionicLoading.hide();
+                $scope.scanEnd();
             });
-            $timeout(function() {
-                ble.stopScan(function() {
-                    $ionicLoading.hide();
-                });
-            }, 10000);
+            $timeout(function () {
+                $scope.scanEnd();
+            }, Conf.SCAN_TIMEOUT * 1000);
+        };
+        $scope.scanEnd = function() {
+            $ionicLoading.hide();
+            if ($scope.bleList.length == 0) {
+                $scope.show = true;
+            }
         };
         $scope.ok = function () {
             BleManager.addBle($scope.selectedBle);
